d9bfa28034c9a178fa583c1c72fe91933ec1fc8f
Nikolai Kleiman <nkleiman@puppet-agent.local>
2016-11-14 23:21:07 +0200

    complete install script

--- agent_install.sh
+++ agent_install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 apt-get update 
 apt-get -y install puppet git
+
 git clone https://github.com/nickatfi/wksenvset
 
+service puppet stop
+cp -f etc.hosts /etc/hosts
+cp -f puppet.agent.conf /etc/puppet/puppet.conf
+
+puppet agent –enable
+service puppet start
